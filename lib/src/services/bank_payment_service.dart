18d5624c77db636442dd06f9b0bb118ddef79e0a
mercyjae <famakinwamercy20@gmail.com>
2024-12-11 18:22:26 -0800

    feat(service): add bank payment service to intialize payment and fetch banks

---
+++ lib/src/services/bank_payment_service.dart
@@ -0,0 +1,87 @@
+import 'package:mind_paystack/src/client/mind_paystack_client.dart';
+import 'package:mind_paystack/src/config/retry_policy.dart';
+import 'package:mind_paystack/src/model/bank_model.dart';
+import 'package:mind_paystack/src/ui/app_api_endpoints.dart';
+
+abstract class BankPaymentService {
+  Future<Map<String, dynamic>> initializePayment({
+    required String accountNumber,
+    required String bankCode,
+    required double amount,
+    required String currency,
+    String? email,
+    Map<String, dynamic>? metadata,
+  });
+
+  Future<Map<String, dynamic>> verifyPayment(String paymentReference);
+
+  Future<Map<String, dynamic>> getPaymentStatus(String paymentId);
+
+  Future<List<BankResponse>> fetchBanks();
+}
+
+class BankPaymentServiceImpl implements BankPaymentService {
+  BankPaymentServiceImpl({
+    required RetryPolicy retryPolicy,
+    required MindPaystackClient mindPaystackClient,
+  })  : _mindPaystackClient = mindPaystackClient,
+        _retryPolicy = retryPolicy;
+  final MindPaystackClient _mindPaystackClient;
+  final RetryPolicy _retryPolicy;
+
+  @override
+  Future<Map<String, dynamic>> initializePayment({
+    required String accountNumber,
+    required String bankCode,
+    required double amount,
+    required String currency,
+    String? email,
+    Map<String, dynamic>? metadata,
+  }) async {
+    final response = await _mindPaystackClient.post<Map<String, dynamic>>(
+      AppApiEndpoints.initializePayment,
+      data: {
+        'account_number': accountNumber,
+        'bank_code': bankCode,
+        'amount': amount,
+        'currency': currency,
+        'email': email,
+        'metadata': metadata,
+      },
+    );
+    return response;
+  }
+
+  @override
+  Future<Map<String, dynamic>> verifyPayment(String paymentReference) async {
+    final response = await _mindPaystackClient.get<Map<String, dynamic>>(
+      AppApiEndpoints.verifyPayment,
+      queryParams: {'paymentReference': paymentReference},
+    );
+    return response;
+  }
+
+  @override
+  Future<Map<String, dynamic>> getPaymentStatus(String paymentId) async {
+    final response = await _mindPaystackClient.get<Map<String, dynamic>>(
+      AppApiEndpoints.getPaymentStatus,
+      queryParams: {'paymentId': paymentId},
+    );
+
+    return response;
+  }
+
+  @override
+  Future<List<BankResponse>> fetchBanks() async {
+    final response = await _mindPaystackClient.get<Map<String, dynamic>>(
+      AppApiEndpoints.getBanks,
+    );
+    final transactions = response['data'] as List<dynamic>;
+    return transactions
+        .map(
+          (json) =>
+              BankResponse.fromJson(Map<String, dynamic>.from(json as Map)),
+        )
+        .toList();
+  }
+}
