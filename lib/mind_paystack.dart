7729da37afa38abe9456e525397ebb117587e1c5
Ayotomide Babalola <babalolagbogo@gmail.com>
2025-01-16 17:51:59 +0100

    feat: initialize dart_example project with basic structure and mind_paystack service

---
+++ lib/mind_paystack.dart
@@ -0,0 +1,16 @@
+import 'dart:developer';
+
+///
+class MindPaystack {
+  String baseUrl = '';
+
+  ///
+  void initialize(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  ///
+  void createTransaction() {
+    print('CREATE TRANSACTION ::::::: Creating Transaction');
+  }
+}
