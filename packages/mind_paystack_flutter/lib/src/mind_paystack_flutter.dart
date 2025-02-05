9899c8c86bba8cb88251b4f6fd86d4fd1281ce37
Ayotomide <Babalolagbogo@gmail.com>
2025-02-05 06:54:48 +0100

    feat: initialize mind_paystack_flutter package with basic structure and configuration files (#22)

---
+++ packages/mind_paystack_flutter/lib/src/mind_paystack_flutter.dart
@@ -0,0 +1,7 @@
+/// {@template mind_paystack_flutter}
+/// My new Flutter package
+/// {@endtemplate}
+class MindPaystackFlutter {
+  /// {@macro mind_paystack_flutter}
+  const MindPaystackFlutter();
+}
---
+++ packages/mind_paystack_flutter/test/src/mind_paystack_flutter_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mind_paystack_flutter/mind_paystack_flutter.dart';
+
+void main() {
+  group('MindPaystackFlutter', () {
+    test('can be instantiated', () {
+      expect(MindPaystackFlutter(), isNotNull);
+    });
+  });
+}
