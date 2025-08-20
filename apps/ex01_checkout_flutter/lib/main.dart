947bd225c11b37603cd2519f5c1c4f1fb36db3f2
Ayotomide Babalola <babalolagbogo@gmail.com>
2025-08-20 03:43:00 +0100

    feat: implement comprehensive checkout page with MindPaystack integration
    - Add a new CheckoutPage widget that facilitates payment processing using the MindPaystack SDK.
    - Include form validation for email and amount inputs, ensuring a smooth user experience.
    - Integrate WebView for handling payment processing and verification.
    - Update main.dart to load environment variables and set the CheckoutPage as the home widget.
    - Enhance pubspec.yaml with necessary dependencies and configure .env for sensitive keys.
    - Create a Podfile for iOS support and include necessary CocoaPods for the project.
    - Add unit tests for the CheckoutPage to ensure functionality and reliability.

--- apps/ex01_checkout_flutter/lib/main.dart
+++ apps/ex01_checkout_flutter/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'checkout_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const CheckoutFlutterApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CheckoutFlutterApp extends StatelessWidget {
+  const CheckoutFlutterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'MindPaystack Flutter Checkout',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CheckoutPage(),
     );
   }
 }
