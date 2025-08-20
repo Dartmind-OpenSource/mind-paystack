0691509ffae2942739632dca5621be38ed69c767
Ayotomide Babalola <babalolagbogo@gmail.com>
2025-08-20 03:55:36 +0100

    chore: remove sample_payment_dart application and related files
    - Deleted the entire sample_payment_dart application, including its configuration files, source code, and assets.
    - This cleanup helps streamline the project by removing unused components.

--- apps/sample_payment_dart/bin/sample_payment_dart.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sample_payment_dart/sample_payment_dart.dart'
-    as sample_payment_dart;
-
-void main(List<String> arguments) {
-  // print('Hello world: ${sample_payment_dart.calculate()}!');
-  sample_payment_dart.SamplePaymentDart().initialize();
-}
