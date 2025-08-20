5ec1eb4cbd8a7eecd3f6837e90307c7468998ba2
Ayotomide Babalola <babalolagbogo@gmail.com>
2025-08-20 16:21:27 +0100

    refactor: streamline dependency registrations in injectable.config.dart
    - Added a comment to specify dart formatting width for better code readability.
    - Removed commented-out factory methods for charge, payment method, and payment channel services to enhance clarity and focus on active dependencies.
    - Improved overall organization of the dependency registration process.

--- packages/mind_paystack/lib/src/core/di/injectable.config.dart
+++ packages/mind_paystack/lib/src/core/di/injectable.config.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
@@ -12,19 +13,8 @@ import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
 import '../../../mind_paystack.dart' as _i10;
-import '../../features/charge/repositories/charge_repository.dart' as _i79;
-import '../../features/charge/services/charge_service.dart' as _i208;
-import '../../features/payment_channels/repositories/payment_channel_repository.dart'
-    as _i173;
-import '../../features/payment_channels/services/payment_channel_service.dart'
-    as _i775;
-import '../../features/payment_methods/repositories/payment_method_repository.dart'
-    as _i122;
-import '../../features/payment_methods/services/payment_method_service.dart'
-    as _i376;
 import '../../features/transaction/repositories/transaction_repository.dart'
     as _i954;
-import '../../features/transaction/services/transaction_service.dart' as _i894;
 import '../network/http_client.dart' as _i1069;
 import 'register_module.dart' as _i291;
 
@@ -45,47 +35,11 @@ _i174.GetIt init(
     _,
   ) =>
       registerModule.provideHttpClient(config));
-  gh.factoryParam<_i122.PaymentMethodRepository, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.paymentMethodRepository(config));
-  gh.factoryParam<_i79.ChargeRepository, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.chargeRepository(config));
   gh.factoryParam<_i954.TransactionRepository, _i10.PaystackConfig, dynamic>((
     config,
     _,
   ) =>
       registerModule.transactionRepository(config));
-  gh.factoryParam<_i173.PaymentChannelRepository, _i10.PaystackConfig, dynamic>(
-      (
-    config,
-    _,
-  ) =>
-          registerModule.paymentChannelRepository(config));
-  gh.factoryParam<_i376.PaymentMethodService, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.paymentMethodService(config));
-  gh.factoryParam<_i894.TransactionService, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.transactionService(config));
-  gh.factoryParam<_i208.ChargeService, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.chargeService(config));
-  gh.factoryParam<_i775.PaymentChannelService, _i10.PaystackConfig, dynamic>((
-    config,
-    _,
-  ) =>
-      registerModule.paymentChannelService(config));
   return getIt;
 }
 
